0e9cac6a7d892d7ab5a3fa2064a34756c10f01e0
marcos tataje <fernando.app.dev@gmail.com>
2021-09-02 10:58:27 +0200

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman', 'Hulk', 'Ironman', 'Thor'];
+  heroeBorrado: string = '';
+
+  borrarHeroe(): void {
+    this.heroeBorrado = this.heroes.shift() || '';
+    console.log(this.heroeBorrado)
+  }
+
+}
